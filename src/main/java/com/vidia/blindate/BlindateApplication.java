b08be2b198fd95aff940f67932f176edb4a80043
Nicholas Hrytsiuk <gritsuk57@gmail.com>
2020-04-05 18:28:18 +0300

    Initial commit

---
+++ src/main/java/com/vidia/blindate/BlindateApplication.java
@@ -0,0 +1,13 @@
+package com.vidia.blindate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vidia/blindate/BlindateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidia.blindate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
